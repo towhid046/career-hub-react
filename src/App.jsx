efbcb76a76eea059eb2d30161e16273fbda7502d
towhid046 <towhidmorol46@gmail.com>
2024-03-22 21:39:27 +0600

    added navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home/Home";
+import AppliedJobs from "./pages/AppliedJobs/AppliedJobs";
+import Blogs from "./pages/Blog/Blogs";
+
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout/>,
+      errorElement: <div className="text-4xl font-bold">404 not found</div>,
+      children: [
+        {
+          path: '/',
+          element: <Home/>
+        },
+        {
+          path: '/appliedjobs',
+          element: <AppliedJobs/>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs/>
+        }
+      ]
+    }
+  ])
   return (
     <div>
-      <h1 className="">hello bd</h1>
+      <RouterProvider router={router}/>
     </div>
   );
 };
---
+++ src/pages/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,10 @@
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            Applied Jobs
+        </div>
+    );
+};
+
+export default AppliedJobs;
