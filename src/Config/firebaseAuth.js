1b18a7df99591ab3b9b9bc5400aae5b6e23c8fc1
towhid046 <towhidmorol46@gmail.com>
2024-03-28 22:45:01 +0600

    added authentication by create user with email and password

---
+++ src/Config/firebaseAuth.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsVo0G6Er9OjLLryqP5byDS4AH38ysQ6U",
+  authDomain: "career-hub-cbcf0.firebaseapp.com",
+  projectId: "career-hub-cbcf0",
+  storageBucket: "career-hub-cbcf0.appspot.com",
+  messagingSenderId: "71964346105",
+  appId: "1:71964346105:web:a0eb07a9573f0e61f8c428",
+  measurementId: "G-Q31N7XNH0K"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,37 @@
 const googleLogo = `https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png`;
 const gitHubLogo = `https://cdn.icon-icons.com/icons2/2428/PNG/512/github_black_logo_icon_147128.png`;
 
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../Config/firebaseAuth";
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const logIn = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="h-[90vh] flex justify-center items-center">
-      <form
-        action=""
-        className="space-y-2 text-center pt-10 px-10 pb-6 bg-white border rounded-lg"
-      >
+      <form className="space-y-2 text-center pt-10 px-10 pb-6 bg-white border rounded-lg">
         <input
           required
-          name="email"
           type="email"
+          onChange={(e) => setEmail(e.target.value)}
           className="border-2 rounded-lg input-md outline-none"
           placeholder="Email address"
         />{" "}
         <br />
         <input
           required
-          name="password"
+          onChange={(e) => setPassword(e.target.value)}
           type="password"
           className="border-2 rounded-lg input-md outline-none"
           placeholder="Password"
@@ -26,6 +39,7 @@ const Login = () => {
         <br />
         <input
           type="submit"
+          onClick={logIn}
           value={"Sign Up"}
           className="btn  bg-black text-white input-md hover:bg-gray-900"
         />{" "}
@@ -33,8 +47,16 @@ const Login = () => {
         <p className="text-2xl font-semibold">Or</p>
         <p className="border-b-2 pb-2">Sign up with</p>
         <figure className="flex gap-8 pt-2 justify-center text-center">
-          <img className="w-8 cursor-pointer rounded-full" src={googleLogo} alt="" />
-          <img className="w-8 cursor-pointer rounded-full" src={gitHubLogo} alt="" />
+          <img
+            className="w-8 cursor-pointer rounded-full"
+            src={googleLogo}
+            alt=""
+          />
+          <img
+            className="w-8 cursor-pointer rounded-full"
+            src={gitHubLogo}
+            alt=""
+          />
         </figure>
       </form>
     </div>
